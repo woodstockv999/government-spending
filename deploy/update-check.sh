34f135da76e8943842309e04490da6eb09744f7d
woodstockv999 <jsbseven170@gmail.com>
2026-07-01 07:46:34 +0900

    auto: update deploy/update-check.sh

--- deploy/update-check.sh
+++ deploy/update-check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 exec "$HOME/deploy/auto-deploy.sh" \
   "marumie-jp" \
-  "$HOME/marumie-jp" \
+  "$HOME/apps/marumie-jp" \
   "marumie-jp" \
   "npm ci && npm run build:flows && NEXT_BASE_PATH=/marumie npm run build" \
   "http://210.131.212.62/marumie"
